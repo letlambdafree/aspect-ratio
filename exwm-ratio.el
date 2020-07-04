558bec0d527c3b267aff95b9954fce6f09ead766
letlambdafree <formeu2s@gmail.com>
2020-07-04 11:33:20 +0900

    add exwm-ratio.el

---
+++ exwm-ratio.el
@@ -0,0 +1,63 @@
+;;; exwm-ratio.el --- resize a window in exwm   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020 by Taeseong Ryu
+
+;; Author: Taeseong Ryu <formeu2s@gmail.com>
+;; URL: https://github.com/letlambdafree/exwm-ratio
+;; Version: 0.0.1
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Shows buffers in a window grid with miniaturized previews from which you
+;; can select a buffer to switch to. Inspired by the exposé feature of some
+;; window managers.
+
+;;; Code:
+
+;; test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(provide 'exwm-ratio)
+;;; exwm-ratio.el ends here
